a7d71f9299d53142883740bf4b0847d6aa64b5b7
Alexis Lui <alexislui@hotmail.co.uk>
2017-07-23 13:37:27 +0100

    package.json taken out for more straightforward npm i tests passing use of promises but perhaps not in the way you intended

--- bcrypt-exercise/index.js
+++ bcrypt-exercise/index.js
@@ -2,17 +2,56 @@
 
 const bcrypt = require('bcryptjs');
 
-const hashPassword = (password) => {
+// const hashPassword = password => {
+//   // use bcrypt to hash the password and return it asynchronously
+//
+//   bcrypt.hash(password, 10, function(err, hash) {
+//     console.log('hash:', hash);
+//   });
+//
+//   return Promise.resolve(hash);
+// };
+
+const hashPassword = password => {
   // use bcrypt to hash the password and return it asynchronously
-  return Promise.resolve('hashed password?');
+  return new Promise((resolve, reject) => {
+    bcrypt.hash(password, 10, function(err, hash) {
+      console.log('hash:', hash);
+      if (err) {
+        reject(err);
+      } else {
+        resolve(hash);
+      }
+    });
+  });
 };
 
+// const comparePasswords = (password, hashedPassword) => {
+//   // use bcrypt to compare the passwords and return a boolean asynchronously
+//   console.log('compare:', bcrypt.compareSync(password, hashedPassword));
+//   // return Promise.resolve('oops');
+// };
+
 const comparePasswords = (password, hashedPassword) => {
-  // use bcrypt to compare the passwords and return a boolean asynchronously
-  return Promise.resolve('oops');
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, hashedPassword, function(err, bool) {
+      console.log(bool);
+      if (err) {
+        reject(err);
+      } else {
+        resolve(bool);
+      }
+    });
+  });
 };
+
+// hashPassword('password');
+// comparePasswords(
+//   'password',
+//   '$2a$10$1yjDJag20y1oJfUoFXq6kOZ/0oYolU7ryOKTZOsS7lDa6vts8/omq'
+// );
 
 module.exports = {
   comparePasswords,
-  hashPassword
+  hashPassword,
 };
