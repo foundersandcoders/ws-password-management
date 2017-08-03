cecdd976348eb305ccde22631298d1a5e72b85e1
jsms90 <jsms@hotmail.co.uk>
2017-08-03 16:54:44 +0100

    remove promises from index.js
    relates #2

--- bcrypt-exercise/index.js
+++ bcrypt-exercise/index.js
@@ -4,12 +4,10 @@ const bcrypt = require('bcryptjs');
 
 const hashPassword = (password) => {
   // use bcrypt to hash the password and return it asynchronously
-  return Promise.resolve('hashed password?');
 };
 
 const comparePasswords = (password, hashedPassword) => {
   // use bcrypt to compare the passwords and return a boolean asynchronously
-  return Promise.resolve('oops');
 };
 
 module.exports = {
